31a56ee81df0af35710bc6fafeb10fce001a4802
Gil <gillederman@gmail.com>
2022-05-22 13:31:27 +0000

    fix typo

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class NEBULA_CONF:
         self.ARANGO_PROXY_PORT = os.getenv('ARANGO_PROXY_PORT', '80')
         self.ARANGO_DB = os.getenv('ARANGO_DB', "prodemo")
         self.PLAYGROUND_DB = os.getenv('ARANGO_DB', "nebula_playground")
-        self.PRODEMO_DB = os.getenv('ARANGO_DB', "prpdemo")
+        self.PRODEMO_DB = os.getenv('ARANGO_DB', "prodemo")
         self.DEFAULT_ARANGO_USER = os.getenv('DEFAULT_ARANGO_USER')
         self.DEFAULT_ARANGO_PASSWORD = os.getenv('DEFAULT_ARANGO_PASSWORD')
         self.ELASIC_HOST = os.getenv('ELASIC_HOST', 'http://tnnb2_master:NeBuLa_2@ec2-18-158-123-0.eu-central-1.compute.amazonaws.com:9200/')
