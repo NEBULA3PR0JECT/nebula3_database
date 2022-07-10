5908f02d8e7799351aee3f2dbcdc7e45f816987b
aharonamir <aharon.amir@gmail.com>
2022-07-10 17:40:29 +0300

    add info and dependencies to database package

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='nebula3_database',
     packages=find_packages(),
-    install_requires=[], # add any additional packages that
-    # needs to be installed along with your package. Eg: 'caer'
+    install_requires=[
+        "numpy",
+        "pymilvus==1.1.1",
+        "python-arango==7.2.0"
+    ], # add any additional packages that
+    # needs to be installed along with your package. Eg: ''
     description='DB adaptor for nebula3',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/NEBULA3PR0JECT/nebula3_database',
     author='Dima',
     author_email='dsivov@gmail.com',
